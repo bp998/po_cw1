09fc83b17735af96f76cdf023be9259aacca3b3b
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2024-04-13 22:06:17 +0200

    Add project files.

---
+++ Lab8 Bartosz Potyrała/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab8_Bartosz_Potyrała
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek liczba1 = new Ulamek(3,4);
+            Ulamek liczba2 = new Ulamek(1,2);
+
+            Console.WriteLine("liczba1 = " + liczba1.ToString());
+            Console.WriteLine("liczba2 = " + liczba2.ToString());
+
+
+            Ulamek mnozenie = liczba1 * liczba2;
+            Ulamek dzielenie = liczba1 / liczba2;
+            Ulamek dodawanie = liczba1 + liczba2;
+            Ulamek odejmowanie = liczba1 - liczba2;
+
+            Console.WriteLine("wynik mnozenia = " + mnozenie.ToString());
+            Console.WriteLine("wynik dzielenia = " + dzielenie.ToString());
+            Console.WriteLine("wynik dodawanie = " + dodawanie.ToString());
+            Console.WriteLine("wynik odejmowanie = " + odejmowanie.ToString());
+        }
+    }
+}
---
+++ Lab8 Bartosz Potyrała/Ulamek.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+namespace Lab8_Bartosz_Potyrała
+{
+    struct Ulamek
+    {
+        int licznik;
+        int mianownik;
+
+        public Ulamek(int inLicznik, int inMianownik)
+        {
+            licznik = inLicznik;
+            mianownik = inMianownik;
+
+            if (inMianownik == 0)
+            {
+                throw new Exception("Mianownik nie moze wynosić 0");
+            }
+
+            Uprosc();
+        }
+        void Uprosc()
+        {
+            int nwd = NWD(Math.Abs(licznik), Math.Abs(mianownik));
+            licznik /= nwd;
+            mianownik /= nwd;
+        }
+            // nwd - najwiekszy wspolny dzielnik
+        int NWD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return (licznik.ToString() + "/" + mianownik.ToString());
+        }
+
+        public static Ulamek operator * (Ulamek a, Ulamek b)
+        {
+            Ulamek toReturn = new Ulamek(a.licznik * b.licznik , a.mianownik * b.mianownik);
+            return (toReturn);
+        }
+
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            Ulamek toReturn = new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+            return (toReturn);
+        }
+
+        
+        public static Ulamek operator + (Ulamek a, Ulamek b)
+        {
+            Ulamek toReturn = new Ulamek(a.licznik * b.mianownik + b.licznik * a.mianownik , a.mianownik * b.mianownik);
+            return (toReturn);
+        }
+        
+        public static Ulamek operator - (Ulamek a, Ulamek b)
+        {
+            Ulamek toReturn = new Ulamek(a.licznik * b.mianownik - b.licznik * a.mianownik, a.mianownik * b.mianownik);
+            return (toReturn);
+        }
+
+        public static bool operator == (Ulamek a, Ulamek b)
+        {
+            return (a.licznik==b.licznik && a.mianownik==b.mianownik);
+        }
+
+        public static bool operator !=(Ulamek a, Ulamek b)
+        {
+            return (a.licznik != b.licznik || a.mianownik != b.mianownik);
+        }
+    }
+}
